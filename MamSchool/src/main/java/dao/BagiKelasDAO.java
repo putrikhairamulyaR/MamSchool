01446755881e80b365169173cef61f6f57eb0f9f
raisalh <147751083+raisalh@users.noreply.github.com>
2024-12-07 23:45:30 +0700

    blom ada show all

--- MamSchool/src/main/java/dao/BagiKelasDAO.java
+++ MamSchool/src/main/java/dao/BagiKelasDAO.java
@@ -2,6 +2,8 @@ package dao;
 
 import classes.JDBC;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 /**
  *
  * @author Raisa Lukman Hakim
@@ -87,4 +89,42 @@ public class BagiKelasDAO {
 
         return count;
     }
+    
+    public List<BagiKelasDAO> getStudentsByTingkatAndMajor(String tingkat, String major) {
+        String query = "SELECT s.id, s.name, s.age, s.gender, c.name as class_name, c.major " +
+                       "FROM students s " +
+                       "JOIN classes c ON s.class_id = c.id " +
+                       "WHERE c.name LIKE ? AND c.major = ?";
+        List<BagiKelasDAO> students = new ArrayList<>();
+
+        try (Connection connection = JDBC.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+
+            if (connection == null) {
+                throw new SQLException("Failed to establish a database connection.");
+            }
+
+            preparedStatement.setString(1, tingkat + "%");
+            preparedStatement.setString(2, major);
+
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                while (resultSet.next()) {
+                    BagiKelasDAO student = new BagiKelasDAO();
+                    student.setId(resultSet.getInt("id"));
+                    student.setName(resultSet.getString("name"));
+                    student.setAge(resultSet.getInt("age"));
+                    student.setGender(resultSet.getString("gender"));
+                    student.setClassName(resultSet.getString("class_name"));
+                    student.setMajor(resultSet.getString("major"));
+                    students.add(student);
+                }
+            }
+        } catch (SQLException e) {
+            System.err.println("Error fetching students by tingkat and major: " + e.getMessage());
+            e.printStackTrace();
+        }
+
+        return students;
+    }
+
 }
